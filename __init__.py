bdf1e19b57c12ad384be887f261f47bde37add10
ImprezaRSC <46632364+ImprezaRSC@users.noreply.github.com>
2019-02-09 22:30:49 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ class Turtleright(MycroftSkill):
     @intent_file_handler('turtleright.intent')
     def handle_turtleright(self, message):
         self.speak_dialog('turtleright')
-        s = "rostopic pub /turtle1/cmd_vel geometry_msgs/Twist -r -- '[2.0, 0.0, 0.0]' '[0.0, 0.0, -1.8]'"
+        s = "rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[2.0, 0.0, 0.0]' '[0.0, 0.0, -1.8]'"
         subprocess.call([s],shell=True)
 
 def create_skill():
